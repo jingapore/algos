12d9565f70dc19328855ef084b920d35003a3b70
jingapore <ngjingsong@gmail.com>
2025-12-31 15:41:13 +0800

    snapshot.

--- knuth-permutation-algo-L/src/main.cpp
+++ knuth-permutation-algo-L/src/main.cpp
@@ -1,3 +1,4 @@
+#ifdef __EMSCRIPTEN__
 #include <cstdint>
 
 // extern C so that compiler doesn't mangle names
@@ -5,14 +6,18 @@ extern "C" {
 // extern signals to compiler that __heap_base is provided somewhere else, in
 // this case link time
 extern unsigned char __heap_base; // tiny bump allocator
+                                  // todo: find out where in the docs is __heap_base exported
 static uintptr_t bump = (uintptr_t)&__heap_base;
 void *alloc(int size) {
   uintptr_t p = bump;
-  // we round up to neartest 8 bits
+  // we round up to nearest 8 bytes 
   // ~7 is mask of lower 3 bits
   bump += (size + 7) & ~7;
   return (void *)p;
 }
+// TODO: write free memory
+}
+#endif
 
 // events are triples of int32: {code,i,j}
 // code: 1=L1 compare, 2=L2 compare, 3=L3 swap, 4=L4 swap, 9=done
@@ -76,5 +81,4 @@ int next_perm_trace(int *data, int n, int *out, int out_cap_triples) {
   }
   push(9, 0, 0);
   return count;
-}
 }
