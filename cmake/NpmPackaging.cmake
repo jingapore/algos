fb89ca74e1f18721d558b39fa3f7ebd64777fb83
njs <ngjingsong@gmail.com>
2026-01-28 02:05:29 +0800

    fix package.json.in issues.

--- cmake/NpmPackaging.cmake
+++ cmake/NpmPackaging.cmake
@@ -5,15 +5,16 @@ function(generate_npm_package TARGET_NAME)
     message(
       FATAL_ERROR "generate_npm_package: ${TARGET_NAME} is not a valid target.")
   endif()
-  get_and_validate_target_property(MAIN_FILE ${TARGET_NAME} NPM_KEY_MAIN)
-  get_and_validate_target_property(TYPES_FILE ${TARGET_NAME} NPM_KEY_TYPES)
-  get_and_validate_target_property(WASM_FILE ${TARGET_NAME} NPM_KEY_WASM)
-
+  get_and_validate_target_property(NPM_KEY_MAIN ${TARGET_NAME} NPM_KEY_MAIN)
+  get_and_validate_target_property(NPM_KEY_TYPES ${TARGET_NAME} NPM_KEY_TYPES)
+  get_and_validate_target_property(NPM_KEY_WASM ${TARGET_NAME} NPM_KEY_WASM)
   set(QUOTED_LIST "")
   list(APPEND QUOTED_LIST ${NPM_KEY_MAIN} ${NPM_KEY_TYPES} ${NPM_KEY_WASM})
   list(TRANSFORM QUOTED_LIST PREPEND "\"")
   list(TRANSFORM QUOTED_LIST APPEND "\"")
   string(JOIN ", " PKG_JSON_FILES_LIST ${QUOTED_LIST})
+
+  set(NPM_SCOPE "@jingapore")
 
   configure_file("${CMAKE_CURRENT_SOURCE_DIR}/package.json.in"
                  "${CMAKE_CURRENT_SOURCE_DIR}/package.json" @ONLY)
