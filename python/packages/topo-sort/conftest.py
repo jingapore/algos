412e20ea8fd95c21ea7b8fd83f031027c6960f69
jingapore <ngjingsong@gmail.com>
2026-04-21 01:24:41 +0800

    add two roots test.

--- python/packages/topo-sort/conftest.py
+++ python/packages/topo-sort/conftest.py
@@ -2,5 +2,9 @@ import pytest
 
 
 @pytest.fixture
-def graph_a() -> list[list[int]]:
+def graph_single_root() -> list[list[int]]:
     return [[1, 2], [], [3, 4], [], []]
+
+@pytest.fixture
+def graph_two_roots() -> list[list[int]]:
+    return [[1, 2], [], [3, 4], [], [], [1]]
