d541af78ba868892fb378b6aec81918ef206af5e
jingapore <ngjingsong@gmail.com>
2026-05-03 23:48:49 +0800

    Feat/str matching (#6)
    * add kmp.

---
+++ python/packages/string-algos/src/string_algos/kmp.py
@@ -0,0 +1,47 @@
+from collections.abc import Iterable
+
+
+def kmp_search(s: str, pat: str) -> Iterable[int]:
+    prefix_map = get_prefix_map(pat)
+    s_len = len(s)
+    pat_len = len(pat)
+    pat_match_len = 0
+
+    for s_idx in range(s_len):
+        while pat_match_len > 0 and s[s_idx] != pat[pat_match_len]:
+            # prefix_map returns len so pat_idx will land us at the right idx
+            pat_match_len = prefix_map[pat_match_len - 1]
+
+        if s[s_idx] == pat[pat_match_len]:
+            pat_match_len += 1
+            if pat_match_len == pat_len:
+                yield s_idx - pat_len + 1
+                pat_match_len = prefix_map[-1]
+
+
+# map returns length of prefix, not index, that matches suffix ending with map[i] (ending is inclusive)
+def get_prefix_map(s: str) -> list[int]:
+    n = len(s)
+    # pi stores the length, not index, of the prefix that matches the suffix and s[i]
+    pi: list[int] = [0] * n
+    # k is the length of the matching prefix for the suffix that ends with (i-1) within the for loop
+    k = 0
+    # start at 1 instead of 0 because when i == 0 p[i] = 0
+    for i in range(1, n):
+        while k > 0 and s[k] != s[i]:
+            # (k-1) instead of k because at this point we claim that prefix of length k matches up to suffix ending with (i-1)
+            # so now we are at suffix[i] and we realise there's no match, and we want to wind back
+            # meaning the prefioux suffix, i.e. suffix[i-1] had a match and we want to "relax" that down, which means we look at
+            # pi[k-1] for that suffix
+            # one invariant is that s[i] == s[pi[i]-1] (-1 is to handle 0 indexing) for pi[i] > 0
+            k = pi[k - 1]
+        # as tempting as it is, note that we can't substitute (s[k] == s[i]) with (k != 0) because that fails when we start this iteration of for i in range(1, n) with k == 0 or when we start with k > 0 but k is driven down to 0 within the while loop
+        if s[k] == s[i]:
+            # at this point, we know that s[i-1] is matched by a prefix of length pi[i-1] (this might not be the longest) and also if we reach this condition we can just safely increment k += 1
+            # if there's a match at s[i], we extend that prefix
+            k += 1
+        # we do not need to have this within the (if s[k] == s[i]) loop
+        # if we don't find a prefix, k == 0 and we assign accordingly. if we find one i.e. its length is >= 1 then we just set it accordingly
+        pi[i] = k
+
+    return pi
---
+++ python/packages/string-algos/src/string_algos/tests/test_kmp.py
@@ -0,0 +1,20 @@
+from string_algos.kmp import get_prefix_map, kmp_search
+
+
+def test_get_prefix_map():
+    a = get_prefix_map("aaa")
+    assert a == [0, 1, 2]
+    abcc = get_prefix_map("abccabc")
+    assert abcc == [0, 0, 0, 0, 1, 2, 3]
+    aab = get_prefix_map("aabaaab")
+    assert aab == [0, 1, 0, 1, 2, 2, 3]
+
+
+def test_kmp():
+    abcc_empty_res = kmp_search("", "abccabc")
+    assert list(abcc_empty_res) == []
+    abcc_res = kmp_search("babccabc", "abccabc")
+    assert list(abcc_res) == [1]
+    abcc_res_dual = kmp_search("babccabccabc", "abccabc")
+    assert list(abcc_res_dual) == [1, 5]
+    assert list(kmp_search("aab", "ab")) == [1]
